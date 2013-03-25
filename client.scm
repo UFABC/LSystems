3c16f38d9b4c3eb86ffdb572dc5e16342421f57f
julianobsg <julianobsg@gmail.com>
2013-03-24 23:18:08 -0300

    client and server update

--- client.scm
+++ client.scm
@@ -1,6 +1,39 @@
 (declare (uses tcp))
-(define-values (i o) (tcp-connect "localhost" 4242))
-(read-line i)
-(write-line "Good Bye!" o)
-(print (read-line i))
 
+(define porta 9000)
+
+(define enviar-mensagem
+  (lambda (mensagem)
+    (let-values ( ((in out) (tcp-connect "localhost" porta)))
+      (write-line mensagem out)
+      (print (read-line in))
+      (close-output-port out)
+      )))
+
+(define imprimir-menu
+  (lambda ()
+    (display "1) Criar arquivo svg, dada uma determinada linguagem.")
+    (newline)
+    (display "2) Editar ultima linguagem enviada e criar novo arquivo.")
+    (newline)
+    (display "3)Executar o programa de acordo com um valor N de passos passado.")
+    (newline)
+    (display "Digite sair para sair.")))
+
+(define calcular-resposta 
+  (lambda(entrada)
+    (enviar-mensagem entrada)
+    ))
+
+(define executar
+  (lambda ()
+    (let ((leitura (read)))
+      (if (not (equal? leitura 'sair))
+          (begin
+            (calcular-resposta  leitura)
+            (executar))))))
+
+(begin 
+  (imprimir-menu)
+  (newline)
+  (executar))
--- server.scm
+++ server.scm
@@ -1,11 +1,48 @@
-(declare (uses tcp))
+(use tcp      ;; TCP/IP
+     srfi-18) ;; Threads
+;;(load "draw-svg.scm")
+;;(load "l-systems.scm")
 
-(load "draw-svg.scm")
-(load "l-systems.scm")
+(define porta 9000)
 
-(define l (tcp-listen 4242))
-(define-values (i o) (tcp-accept l))
-(write-line "Hello!" o)
-(print (read-line i))
-(close-input-port i)
-(close-output-port o)
+(define interage
+  (lambda (in out)
+    ;; Manda um olá para o Fulano que conectou,
+    ;; na porta out:
+    (display "Olá, Fulano do outro lado!" out)
+    (newline out)
+    (flush-output out) ;; esvazia o buffer, mandando o que estiver pendente
+    (let ((linha (read-line in))) ;; lê da porta in
+      (display (format "O Fulano disse: ~a~%" linha))))) ;; display LOCAL!
+    ;;(interage in out) tive que deletar essa linha, por que dava pau
+   ;; Poderia entrar em loop e continuar interagindo!
+
+;; Aceita conexão TCP e chama interage.
+(define trata
+  (lambda (s)
+    ;; tcp-accept aceita uma conexão e retorna duas portas,
+    ;; uma de entrada e uma de saída
+    (let-values (((in out) (tcp-accept s)))
+      ;; tcp-addresses devolve dois IPs -- o do servidor e
+      ;; o do cliente que se conectou.
+      (let-values (((meu-ip ip-dele) (tcp-addresses in)))
+        ;; Comece a thread que interage:
+        (thread-start! (make-thread
+                        (lambda ()
+                          (interage in out)
+                          ;; após interagir, fechamos a porta:
+                          (close-output-port out))))))
+
+    ;; Depois de criar uma thread para cuidar da conexão, reinicie
+    ;; (chama trata novamente):
+    (trata s)))
+
+
+;; Pega uma porta, começa a ouvir, e chama o tratador
+;; de socket.
+(define inicia-servidor
+  (lambda ()
+    (let ((socket (tcp-listen porta)))
+      (trata socket))))
+
+(inicia-servidor)
